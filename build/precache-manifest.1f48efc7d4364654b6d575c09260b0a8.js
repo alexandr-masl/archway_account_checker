89ef68073a403061ef48c4a2c1ec3c61674b8dc1
green <masllonishe@gmail.com>
2022-04-20 22:32:58 +0200

    build

---
+++ build/precache-manifest.1f48efc7d4364654b6d575c09260b0a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e45638e5a5ae3f4ef60",
+    "url": "/static/css/main.28624a71.chunk.css"
+  },
+  {
+    "revision": "0e45638e5a5ae3f4ef60",
+    "url": "/static/js/main.0e45638e.chunk.js"
+  },
+  {
+    "revision": "a08c7d9bcdb692cd2648",
+    "url": "/static/js/1.a08c7d9b.chunk.js"
+  },
+  {
+    "revision": "b7fcaaed769f69a9cfd1",
+    "url": "/static/js/2.b7fcaaed.chunk.js"
+  },
+  {
+    "revision": "ed7770fa628ac3e0c3a0",
+    "url": "/static/js/runtime~main.ed7770fa.js"
+  },
+  {
+    "revision": "a3b68a90dd48ae76e2d556f25c85aebf",
+    "url": "/static/media/logo.a3b68a90.svg"
+  },
+  {
+    "revision": "2ac9e59e7fea9e4df50776d215cbc028",
+    "url": "/index.html"
+  }
+];
